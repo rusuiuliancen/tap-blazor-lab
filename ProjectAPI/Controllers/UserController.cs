a1929d81aee09ab70d2a2dcc1a8429c83136658f
Rusu, Iulian <iulian.rusu@centric.eu>
2024-04-17 19:26:55 +0300

    Add project files.

---
+++ ProjectAPI/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("login")]
+        public IActionResult Login(string username, string password)
+        {
+            return Ok(username);
+        }
+    }
+}
